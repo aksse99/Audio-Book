e8ede6b5a3b1bc15dc1d2389e8ad2b87466c458c
Ayush <57259393+aksse99@users.noreply.github.com>
2022-01-25 13:52:39 +0530

    Update audiobook.py

--- audiobook.py
+++ audiobook.py
@@ -1,9 +1,13 @@
 import pyttsx3
 import PyPDF2
 
-
-
+book = open('book.pdf','rb')
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
+print(pages)
 speaker = pyttsx3.init()
-
-speaker.say('text')
-speaker.runAndWait()
+for num in range(0, pages):
+    page = pdfReader.getPage(0)
+    text = page.extractText()
+    speaker.say(text)
+    speaker.runAndWait()
